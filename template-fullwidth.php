7c0762954b8688d12618017f884ff249dcb26ec3
Victor Tihai <victor@tihai.md>
2017-05-24 13:04:20 -0400

    Full Width Page Fix #6

---
+++ template-fullwidth.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Full Width (no sidebar)
+ *
+ * @package WordPress
+ * @subpackage Charitas
+ * @since Charitas Lite 1.0.6
+ */
+?>
+
+<?php get_header(); ?>
+	<div class="item teaser-page-list">
+		<div class="container_16">
+			<aside class="grid_10">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+			</aside>
+			<?php if ( ot_get_option('charitas_breadcrumbs') != "off") { ?>
+				<div class="grid_6">
+					<div id="rootline">
+						<?php charitas_breadcrumbs(); ?>	
+					</div>
+				</div>
+			<?php } ?>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div id="main" class="site-main container_16">
+		<div class="inner">
+			<div id="primary" class="grid_16">
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; // end of the loop. ?>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+<?php get_footer(); ?>
